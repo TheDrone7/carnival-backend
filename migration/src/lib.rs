24b9e883e4ca11bd0779df4843652a5527f0be4a
TheDrone7 <hsbhatia765@gmail.com>
2022-06-09 04:07:45 +0530

    Write game api table migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -4,6 +4,7 @@ mod m20220605_161121_create_user_table;
 mod m20220605_165732_create_game_table;
 mod m20220605_171425_add_user_game_relation;
 mod m20220606_175218_create_game_data;
+mod m20220609_004546_create_game_api_table;
 
 pub struct Migrator;
 
@@ -15,6 +16,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20220605_165732_create_game_table::Migration),
             Box::new(m20220605_171425_add_user_game_relation::Migration),
             Box::new(m20220606_175218_create_game_data::Migration),
+            Box::new(m20220609_004546_create_game_api_table::Migration),
         ]
     }
 }
