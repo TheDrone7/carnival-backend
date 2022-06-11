97d2161b4c13cd688d9e42b159f63f667b2a274e
TheDrone7 <hsbhatia765@gmail.com>
2022-06-11 13:49:41 +0530

    Use API key instead of user auth for game data

--- src/graphql/mutation/game_data.rs
+++ src/graphql/mutation/game_data.rs
@@ -1,7 +1,6 @@
 use crate::graphql::types::game_data::{new_game_data, GameDataType};
 use async_graphql::*;
 use entity::game;
-use entity::game::Entity as Game;
 use entity::game_data;
 use entity::game_data::Entity as GameData;
 use sea_orm::{entity::*, query::*, DatabaseConnection, DeleteResult};
@@ -14,24 +13,16 @@ impl GameDataMutation {
     pub async fn set_game_data<'ctx>(
         &self,
         ctx: &Context<'ctx>,
-        game_id: i32,
         key: String,
         value: String,
     ) -> FieldResult<GameDataType> {
         let db = ctx.data_unchecked::<DatabaseConnection>();
-        let user_id = ctx.data_opt::<String>();
-        if user_id.is_none() {
-            return Err(FieldError::new("Please sign-in with replit first."));
+        let some_game = ctx.data_unchecked::<Option<game::Model>>();
+        if some_game.is_none() {
+            return Err(FieldError::new("Invalid API Key found."));
         }
-        let user_id = user_id.unwrap().parse::<i32>().unwrap();
-        let game: Option<game::Model> = Game::find_by_id(game_id).one(db).await?;
-        if game.is_none() {
-            return Err(FieldError::new("Invalid request, no such game found."));
-        } else if game.unwrap().user_id != user_id {
-            return Err(FieldError::new(
-                "Unauthorized, you are not the owner of this game.",
-            ));
-        }
+        let game_id = some_game.clone().unwrap().id;
+
         let data: Vec<game_data::Model> = GameData::find()
             .filter(game_data::Column::GameId.eq(game_id))
             .filter(game_data::Column::Key.eq(key.clone()))
@@ -39,45 +30,35 @@ impl GameDataMutation {
             .all(db)
             .await
             .expect("Unable to fetch game data");
+
         let mut new_data: game_data::ActiveModel;
-        let result_data: game_data::Model;
-        if data.is_empty() {
-            result_data = new_game_data(GameDataType {
+        let result_data: game_data::Model = if data.is_empty() {
+            new_game_data(GameDataType {
                 game_id,
                 key: key.clone(),
                 value: Some(value),
             })
             .insert(db)
-            .await?;
+            .await?
         } else {
             new_data = data[0].clone().into();
             new_data.value = Set(Some(value));
-            result_data = new_data.update(db).await?;
-        }
+            new_data.update(db).await?
+        };
         Ok(result_data.into())
     }
 
     pub async fn delete_game_data<'ctx>(
         &self,
         ctx: &Context<'ctx>,
-        game_id: i32,
         key: Option<String>,
     ) -> FieldResult<Vec<GameDataType>> {
         let db = ctx.data_unchecked::<DatabaseConnection>();
-        let user_id = ctx.data_opt::<String>();
-        if user_id.is_none() {
-            return Err(FieldError::new("Please sign-in with replit first."));
+        let some_game = ctx.data_unchecked::<Option<game::Model>>();
+        if some_game.is_none() {
+            return Err(FieldError::new("Invalid API Key found."));
         }
-        let user_id = user_id.unwrap().parse::<i32>().unwrap();
-        let game: Option<game::Model> = Game::find_by_id(game_id).one(db).await?;
-        if game.is_none() {
-            return Err(FieldError::new("Invalid request, no such game found."));
-        } else if game.unwrap().user_id != user_id {
-            return Err(FieldError::new(
-                "Unauthorized, you are not the owner of this game.",
-            ));
-        }
-
+        let game_id = some_game.clone().unwrap().id;
         let mut query = GameData::find().filter(game_data::Column::GameId.eq(game_id));
         let some_key = key.clone();
         if let Some(some_key) = some_key {
@@ -90,7 +71,7 @@ impl GameDataMutation {
             .expect("Unable to fetch game data");
 
         if data.is_empty() {
-            return Err(FieldError::new("No such data found."));
+            return Err(FieldError::new("No relevant data found."));
         } else {
             let mut query = GameData::delete_many().filter(game_data::Column::GameId.eq(game_id));
             let some_key = key.clone();
